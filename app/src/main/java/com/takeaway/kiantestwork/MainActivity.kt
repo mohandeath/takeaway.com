86361a6ead891ef733d8d3eaf5fa20faba5ec4f9
mohandeath <diakosco@gmail.com>
2019-11-26 19:22:33 +0330

    + MainActivity is ready for base list showing

--- app/src/main/java/com/takeaway/kiantestwork/MainActivity.kt
+++ app/src/main/java/com/takeaway/kiantestwork/MainActivity.kt
@@ -1,10 +1,17 @@
 package com.takeaway.kiantestwork
 
 import android.os.Bundle
+import android.widget.Toast
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.takeaway.kiantestwork.di.DaggerViewModelFactory
+import com.takeaway.kiantestwork.ui.adapter.RestauranListAdapter
 import com.takeaway.kiantestwork.viewmodels.RestaurantListViewModel
 import dagger.android.support.DaggerAppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.item_loading.*
 import javax.inject.Inject
 
 class MainActivity : DaggerAppCompatActivity() {
@@ -13,16 +20,53 @@ class MainActivity : DaggerAppCompatActivity() {
     @Inject
     lateinit var viewModelFactory: DaggerViewModelFactory
     lateinit var viewModel: RestaurantListViewModel
+    private lateinit var layoutManager: RecyclerView.LayoutManager
+    private lateinit var adapter: RestauranListAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupViews()
         setupViewModel("statuss")
+        observeChanges()
     }
 
-    private fun setupViewModel(sortType:String){
-        viewModel = ViewModelProviders.of(this, viewModelFactory).get(RestaurantListViewModel::class.java)
+    private fun setupViewModel(sortType: String) {
+        viewModel =
+            ViewModelProviders.of(this, viewModelFactory).get(RestaurantListViewModel::class.java)
         viewModel.sortType = sortType
 
     }
+
+    private fun setupViews() {
+        adapter = RestauranListAdapter(this) { restaurant ->
+            Toast.makeText(this, "you have clicked on ${restaurant.name}", Toast.LENGTH_LONG).show()
+        }
+        layoutManager = LinearLayoutManager(this)
+        restaurantRecyclerView.adapter = adapter
+        restaurantRecyclerView.layoutManager = layoutManager
+
+    }
+
+    private fun observeChanges() {
+        viewModel.restaurantList.observe(this, Observer {
+            adapter.setItems(it)
+        })
+
+        viewModel.loadingVisibility.observe(this, Observer {
+            loading.visibility = it!!
+        })
+
+        viewModel.retryVisibility.observe(this, Observer {
+            retryBtn.visibility = it!!
+        })
+
+        viewModel.errorMessage.observe(this, Observer {
+            Toast.makeText(this, getString(R.string.str_error_general), Toast.LENGTH_LONG).show()
+
+
+        })
+
+    }
+
 }
---
+++ app/src/main/java/com/takeaway/kiantestwork/ui/adapter/RestauranListAdapter.kt
@@ -0,0 +1,49 @@
+package com.takeaway.kiantestwork.ui.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.takeaway.kiantestwork.R
+import com.takeaway.kiantestwork.dto.Restaurant
+
+class RestauranListAdapter(
+    private val context: Context,
+    var onClick: (Restaurant) -> Unit
+
+) : RecyclerView.Adapter<RestauranListAdapter.RestaurantViewHolder>() {
+
+    private var restaurants: MutableList<Restaurant> = ArrayList()
+
+
+
+    fun setItems(items: List<Restaurant>) {
+        this.restaurants.clear()
+        this.restaurants.addAll(items)
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RestaurantViewHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.restaurant_item, parent, false)
+        return RestaurantViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return restaurants.size
+    }
+
+    override fun onBindViewHolder(holder: RestaurantViewHolder, position: Int) {
+        val item = restaurants[position]
+        holder.bindView(item)
+        holder.itemView.setOnClickListener { onClick(item) }
+        //TODO : set onclick for favorite
+    }
+
+
+    inner class RestaurantViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        fun bindView(restaurant: Restaurant) {
+
+        }
+    }
+}
